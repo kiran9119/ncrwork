bedcafa0ec303770c98640324bcfd3519ef82c99
kiran9119 <narrasaikiranreddy9119@gmail.com>
2019-03-04 11:16:06 -0800

    thread exit code using terminate function

---
+++ windows internals/thread3.cpp
@@ -0,0 +1,36 @@
+#include<windows.h>
+#include<tchar.h>
+#include<iostream>
+using namespace std;
+DWORD dwexitcode;
+DWORD WINAPI FUNC(LPVOID str)
+{
+	for (BOOL i = 0; i<10; i++)
+		cout << "i=" << i << endl;
+	DWORD dwexitcode = 10;
+
+	return 15;
+}
+
+
+int _tmain()
+{
+	HANDLE H1;
+	H1 = CreateThread(NULL,//Security attribute
+		0,//Stack Size
+		FUNC,//FUNCTION NAME
+		NULL,//Function PArameter
+		CREATE_SUSPENDED,//CREATION FLAG
+		NULL);
+
+	Sleep(2000);
+	ResumeThread(H1);//it resumes thread
+	WaitForSingleObject(H1, INFINITE);
+	DWORD dwexitcode = 0;
+	
+	TerminateThread(H1,dwexitcode);
+	GetExitCodeThread(H1, &dwexitcode);
+	cout << "dwexitcode" << dwexitcode;
+	getchar();
+
+}
